f0c6caa629ff3e1c8a1790a4e486dcf8833e7d99
heejyang <innopratt@gmail.com>
2025-12-31 01:48:01 +0000

    Deploying to gh-pages from @ heejyang/NPClab@82ef20e66183007705b22e8e384a650a70d2bf24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/NPClab/";
     },
-  },{id: "nav-people",
-          title: "people",
-          description: "members of the lab",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/NPClab/people/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
