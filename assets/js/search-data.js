bb7d71e93fd5aee95d8caf12276757d12f147eae
heejyang <innopratt@gmail.com>
2025-12-31 02:18:40 +0000

    Deploying to gh-pages from @ heejyang/NPClab@fabdca4d5644ebf4f61525ef3d12eef9d6daa7d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,11 +424,6 @@ ninja.data = [{
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/NPClab/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/NPClab/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
